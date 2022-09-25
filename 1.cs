6aab8e98dd933b5fed4f53d2ea757f2113eba580
Csh4rpEnjoyer <113981795+Csh4rpEnjoyer@users.noreply.github.com>
2022-09-25 23:51:46 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+Console.WriteLine("Введите x");
+double x= double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k");
+int k = int.Parse(Console.ReadLine());
+double y = 0;
+double w = Math.PI / 180;
+double j = Math.Round(Math.Sin(x*w));
+while (k>0)
+{
+    y = y + j;
+    j = Math.Sin(j*w);;
+    k=k-1;
+}
+Console.WriteLine(y);
---
+++ 2.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+Console.WriteLine("Введите x");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k");
+int k = int.Parse(Console.ReadLine());
+double y = 0;
+double w = Math.PI / 180;
+double j = Math.Round(Math.Cos(x * w));
+double v = Math.Round(Math.Cos(x * w));
+while (k > 0)
+{
+    y = y + j;
+    j = j*v;
+    k = k - 1;
+}
+Console.WriteLine(y);
+
